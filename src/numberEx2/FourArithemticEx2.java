47eea130dc943c27d39a81fb412a09a6c729b784
connection579 <wlswn4118@naver.com>
2020-10-29 02:03:32 +0900

    증감연산자 ++와--예시 연산자의 위치

---
+++ src/numberEx2/FourArithemticEx2.java
@@ -0,0 +1,17 @@
+package numberEx2;
+
+public class FourArithemticEx2 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int j = 10;
+//		i++; // ++는 값 1만큼 증가 시킨다
+//		j--; // --는 값을 1만큼 감소 시킨다
+		
+		System.out.println(i);
+		System.out.println(i++);// i++ 값이 참조된후 증가
+		System.out.println(++i); // ++i 값이 참조되기전에 증가
+		System.out.println(j);
+	}
+
+}
